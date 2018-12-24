a1799a386793a61151e275819093842c45fd3782
infinite harbor <infinite.harbor@gmail.com>
2018-12-23 22:20:54 -0500

    setup

---
+++ GruntFile.js
@@ -0,0 +1,27 @@
+var grunt = require("grunt");
+
+
+grunt.initConfig({
+  bump: {
+    options: {
+      files: ['package.json'],
+      updateConfigs: [],
+      commit: true,
+      commitMessage: 'Release v%VERSION%  [ci skip]',
+      commitFiles: ['package.json'],
+      createTag: true,
+      tagName: 'v%VERSION%',
+      tagMessage: 'Version %VERSION%',
+      push: true,
+      pushTo: 'origin',
+      gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+      globalReplace: false,
+      prereleaseName: false,
+      metadata: '',
+      regExp: false
+    }
+  }
+})
+
+
+grunt.loadNpmTasks('grunt-bump');
